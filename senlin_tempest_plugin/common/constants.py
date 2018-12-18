13087c5e1396e9541f9025a0c8b0634bbe656e96
Duc Truong <dtruong@blizzard.com>
2018-12-18 17:17:42 +0000

    Update cirros image name to match latest devstack
    Update cirros image to 0.3.6
    
    Change-Id: Iee226c3295a79629cb38faf99359194e9bda278d

--- senlin_tempest_plugin/common/constants.py
+++ senlin_tempest_plugin/common/constants.py
@@ -17,7 +17,7 @@ spec_nova_server = {
     "properties": {
         "flavor": "1",
         "name": "new-server-test",
-        "image": "cirros-0.3.5-x86_64-disk",
+        "image": "cirros-0.3.6-x86_64-disk",
         "networks": [
             {"network": "private"}
         ]
